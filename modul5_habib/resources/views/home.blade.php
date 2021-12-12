899a581d095c79aca2d5d3fa4a578d91d7d6aa76
M.Habib Jauhari <92283984+mhabibjauhari@users.noreply.github.com>
2021-12-12 23:22:52 +0700

    MODUL5 HABIB

---
+++ modul5_habib/resources/views/home.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MODUL5 HABIB</title>
+</head>
+<body>
+<style>
+body{
+    margin: 0;
+    padding: 0;
+    background: #FAEDF0;
+    font-family: Arial, Helvetica, sans-serif;
+}
+.navbar ul{
+    list-style: none;
+    background: #FAEDF0;
+    padding: 0;
+    margin: 0;
+    text-align: center;
+}
+.navbar li{
+    display: inline-block;
+}
+.navbar a{
+    text-decoration: none;
+    color: #191919;
+    width: 100px;
+    display: block;
+    padding: 25px 20px;
+    font-size: 15px;
+    text-transform: uppercase;
+    font-weight: bold;
+    text-align: center;
+}
+h2{
+    text-align: center;
+}
+</style>
+<div class ="navbar">
+        <ul>
+            <li><a href ="home2.blade.php"><b>Home</b></a></li>
+            <li><a href ="#"><b>Vaccine</b></a></li>
+            <li><a href ="#"><b>Patient</b></a></li>
+        </ul>
+        
+<h2>About Us</h2>
+</body>
+</html>
---
+++ modul5_habib/resources/views/home2.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MODUL5 HABIB</title>
+</head>
+<body>
+<style>
+body{
+    margin: 0;
+    padding: 0;
+    background: #FAEDF0;
+    font-family: Arial, Helvetica, sans-serif;
+}
+.navbar ul{
+    list-style: none;
+    background: #FAEDF0;
+    padding: 0;
+    margin: 0;
+    text-align: center;
+}
+.navbar li{
+    display: inline-block;
+}
+.navbar a{
+    text-decoration: none;
+    color: #191919;
+    width: 100px;
+    display: block;
+    padding: 25px 20px;
+    font-size: 15px;
+    text-transform: uppercase;
+    font-weight: bold;
+    text-align: center;
+}
+h2{
+    text-align: center;
+}
+</style>
+<div class ="navbar">
+        <ul>
+            <li><a href ="#"><b>Home</b></a></li>
+        </ul>
+        
+<h2>About Us</h2>
+
+<p>Vaksin dibuat untuk mencegah</p>
+</body>
+</html>
